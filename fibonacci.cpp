68205131a06b443fd3f6c418edf7b980bda24fc5
Vishal Gupta <vgupata48@gmail.com>
2026-06-03 23:39:40 +0530

    Create fibonacci.cpp
    Added a Fibonacci series program in C++. The program generates Fibonacci numbers up to the user-specified number of terms using a simple loop.

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, c;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
