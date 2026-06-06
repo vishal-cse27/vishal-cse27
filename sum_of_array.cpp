362e51e2977cc871c82b26c08a46f9aa062097c1
Vishal Gupta <vgupata48@gmail.com>
2026-06-06 17:34:54 +0530

    Create sum_of_array.cpp

---
+++ sum_of_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int sum = 0;
+
+    for(int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
